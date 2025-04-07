74ad258d20085904fac3e3d0a0e3b3825e7d9f0c
AuroraHumo <carles.casallachs@gmail.com>
2025-04-07 19:10:52 +0200

    created cards and finished sectiondownloads

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import './styles/buttons.css'
 import './styles/features.css'
 import './styles/tabs.css'
 import './styles/hero.css'
+import './styles/downloads.css'
 
 function App() {
 
--- src/components/sectionDownloads.tsx
+++ src/components/sectionDownloads.tsx
@@ -1,4 +1,7 @@
-import CardComponent from "./cardComponent"
+import CardAtom1 from "./cardAtom1"
+import CardAtom2 from "./cardAtom2"
+import CardAtom3 from "./cardAtom3"
+
 
 const SectionDownloads = () => {
 
@@ -7,8 +10,12 @@ const SectionDownloads = () => {
     <div className="container">
     <h2>Download the extension</h2>
     <p>We've got more browsers in the pipeline. Please do let us know if you've got a favorite you'd like us to prioritize.</p>
-    
-    <CardComponent/>
+    <div className="card-container">
+    <CardAtom1/>
+    <CardAtom2/>
+    <CardAtom3/>
+    </div>
+
 
     </div>
 
