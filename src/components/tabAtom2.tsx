b901ed016267867e5d365b81e6deb9e4b0772048
AuroraHumo <carles.casallachs@gmail.com>
2025-04-07 19:20:32 +0200

    minor tabAtom changes

--- src/components/tabAtom2.tsx
+++ src/components/tabAtom2.tsx
@@ -1,16 +1,16 @@
+import Button from "./buttonComponent";
+
 function TabAtom2() {
   return (
-  <>
-    <div className="half text-center">
-      <img src="src\assets\illustration-features-tab-2.svg" className="img-tab" alt="..."/>
-    </div>
-    <div className="half text-left">
-    <h2>Speedy Searching</h2>
-    <p>Troba els teus marcadors en segons...</p>
-    <button className="button button-blue">More info</button>
-    </div>
-</>
-
+    <>
+      <div className="half text-center">
+        <img src="src\assets\illustration-features-tab-2.svg" className="img-tab" alt="..."/>
+      </div>
+      <div className="half text-left">
+      <h2>Speedy Searching</h2>
+      <p>Find your bookmarks in seconds with our smart search feature.</p>
+      <Button className="button button-blue">More info</Button>      </div>
+  </>
 );
 }
 
