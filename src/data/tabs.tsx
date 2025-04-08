86159cdca195db5dd8b3737b854b329138c4e0a6
AuroraHumo <carles.casallachs@gmail.com>
2025-04-08 12:47:03 +0200

    modified tabsComponent logic

---
+++ src/data/tabs.tsx
@@ -0,0 +1,13 @@
+export const NewTabs: Array<{ title: string; description: string; url: string }> = [{
+    title: 'Bookmark in one click',
+    description: 'Organize your bookmarks however you like. Our drag and drop interface gives you control over how you manage your favourite sites.',
+    url: 'src/assets/illustration-features-tab-1.svg'
+}, {
+    title: 'Speedy Searching',
+    description: 'Find your bookmarks in seconds with our smart search feature.',
+    url: 'src/assets/illustration-features-tab-2.svg'
+}, {
+    title: 'Easy Sharing',
+    description: 'Our powerful sharing tools make it easy to share your favorite sites with friends and family.',
+    url: 'src/assets/illustration-features-tab-3.svg'
+}];
